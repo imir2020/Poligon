7986446bacb653125bab6348b0ee033c94fa75a3
Vladimir <imir2020@yandex.ru>
2022-11-12 17:14:13 +0300

    com21

--- src/algoritms/myAlgoritm1/GenOfRandomString.java
+++ src/algoritms/myAlgoritm1/GenOfRandomString.java
@@ -13,9 +13,9 @@ For filling random symbols for file.txt
 public class GenOfRandomString {
     int rawSize = 32;
     String buffer = "";
-    String[] firstHalfOfstr = new String[rawSize/2];
-    String[] secondHalfOfstr = new String[rawSize/2];
-    String [] mixOfRandom = new String[rawSize];
+    String[] firstHalfOfstr = new String[rawSize / 2];
+    String[] secondHalfOfstr = new String[rawSize / 2];
+    String[] mixOfRandom = new String[rawSize];
 
     public void readFromFile() {
         List<String> listForFile;// = new ArrayList<>();
@@ -37,15 +37,43 @@ public class GenOfRandomString {
         int k = 0;
 
         System.out.println(buffer.length());
-        for (int i = 0; i < firstHalfOfstr.length; i++) {//первая половина строки в массив mixOfRandom
+        for (int i = 0; i < firstHalfOfstr.length; i++) {//первая половина строки в массив firstHalfOfstr
             firstHalfOfstr[i] = String.valueOf(buffer.charAt(i));
         }
-        for (int i =buffer.length()/2; i< buffer.length(); i++) {//вторая половина строки в массив reversMixOfRandom
+        for (int i = buffer.length() / 2; i < buffer.length(); i++) {//вторая половина строки в массив secondHalfOfstr
             secondHalfOfstr[k++] = String.valueOf(buffer.charAt(i));
+        }
+        //реверс массива secondHalfOfstr
+
+        for (int i = 0; i < secondHalfOfstr.length / 2; i++) {
+            String temp = secondHalfOfstr[i];
+
+            secondHalfOfstr[i] = secondHalfOfstr[secondHalfOfstr.length - 1 - i];
+            secondHalfOfstr[secondHalfOfstr.length - 1 - i] = temp;
         }
 
 
+        for (int i = 0; i < firstHalfOfstr.length; i++) {
+            if (i == 0)
+                mixOfRandom[i] = firstHalfOfstr[i];
+            else if (i != 0) mixOfRandom[i * 2] = firstHalfOfstr[i];
 
+        }
+        //int i = 1;
+//        while (i<secondHalfOfstr.length){
+//            if (mixOfRandom[i]!= null){
+//                mixOfRandom[i] = secondHalfOfstr[i];
+//            }
+//        }
+        for (int i = 0; i < secondHalfOfstr.length; i++) {
+            if (i == 0) {
+                int buff = i + 1;
+                mixOfRandom[buff] = secondHalfOfstr[i];
+            } else {
+                mixOfRandom[(i * 2) + 1] = secondHalfOfstr[i];
+            }
+
+        }
 
         System.out.println(buffer);
     }
@@ -68,6 +96,12 @@ public class GenOfRandomString {
         System.out.println();
         for (int i = 0; i < one.firstHalfOfstr.length; i++) {
             System.out.print(one.secondHalfOfstr[i] + " ");
+
+        }
+
+        System.out.println();
+        for (int i = 0; i < one.mixOfRandom.length; i++) {
+            System.out.print(one.mixOfRandom[i] + " ");
 
         }
     }
